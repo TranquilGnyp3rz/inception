03df187a417c3aa7b5cf02accff156486b1c4d6d
heloufra <heloufra@localhost.localdomain>
2023-06-15 13:55:07 -0400

    .

--- src/requirements/wordpress/tools/create.sh
+++ src/requirements/wordpress/tools/create.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sleep 10
-wp core config --allow-root --dbhost="${MYSQL_HOSTNAME}" --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}"
-chmod 600 wp-config.php
 chown -R www-data *
+wp  config create --allow-root --dbhost="${MYSQL_HOSTNAME}" --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}"
+
+echo $(pwd)
 
 wp core install --allow-root --url="${URL_DNS}" --title="${WP_TITLE}" --admin_name="${WP_TITLE}" --admin_password="${WP_ADMIN_PSW}" --admin_email="${WP_ADMIN_EMAIL}"
 wp user create --allow-root ${WP_USR} ${WP_EMAIL} --user_pass=${WP_PWD} --role=${WP_USER_ROLE}
