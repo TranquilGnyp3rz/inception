e3e889cc159e01359a9c2a65ff61bc343db90583
heloufra <heloufra@localhost.localdomain>
2023-06-16 17:46:50 -0400

    .

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,13 @@
 #!bin/sh
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "create database if not exists ${MYSQL_DATABASE};"
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "use mysql;UPDATE user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE user='root';UPDATE user SET plugin = 'mysql_native_password', host = '%' WHERE user = 'root';FLUSH PRIVILEGES;"
 
 
-mysql -h localhost --user=root --database=mysql <<-EOSQL
-create database if not exists ${MYSQL_DATABASE};
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-FLUSH PRIVILEGES;
-UPDATE user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE user='root';
-UPDATE user SET plugin = 'mysql_native_password', host = '%' WHERE user = 'root';
-FLUSH PRIVILEGES;
-EOSQL
-
-mysqld_safe --user=mysql --console
+# echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" | mysql -h localhost -p$MYSQL_ROOT_PASSWORD
+# echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql -h localhost -p$MYSQL_ROOT_PASSWORD
+# echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" | mysql -h localhost -p$MYSQL_ROOT_PASSWORD
+# echo "FLUSH PRIVILEGES;" | mysql -h localhost -p$MYSQL_ROOT_PASSWORD
+# echo "use mysql;UPDATE user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE user='root'; \
+#         UPDATE user SET plugin = '' WHERE user = 'root';FLUSH PRIVILEGES;" | mysql -h localhost -p$MYSQL_ROOT_PASSWORD
--- srcs/requirements/wordpress/tools/create.sh
+++ srcs/requirements/wordpress/tools/create.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+sleep 10
 
-sleep 20 
-
-chown -R www-data *
-
+chown -R www-data /wordpress
+cd /wordpress
+# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+rm -rf wp-config.php
 wp  config create --allow-root --dbhost="${MYSQL_HOSTNAME}" --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}"
-
+chmod +x wp-config.php
 wp core install --allow-root --url="${URL_DNS}" --title="${WP_TITLE}" --admin_name="${WP_TITLE}" --admin_password="${WP_ADMIN_PSW}" --admin_email="${WP_ADMIN_EMAIL}"
 wp user create --allow-root ${WP_USR} ${WP_EMAIL} --user_pass=${WP_PWD} --role=${WP_USER_ROLE}
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
 
 exec php-fpm7.3 -F -R
