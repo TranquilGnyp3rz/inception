c500bcbe1eefb854fd73bc28ebc47ef201b61d02
heloufra <heloufra@localhost.localdomain>
2023-06-15 20:05:31 -0400

    .

---
+++ Makefile
@@ -0,0 +1,12 @@
+all:
+	docker compose -f ./srcs/docker-compose.yml up --build
+build:
+	docker compose -f ./srcs/docker-compose.yml build
+up:
+	docker compose -f ./srcs/docker-compose.yml up
+down:
+	docker compose -f ./srcs/docker-compose.yml down
+fclean:
+	docker system prune -a ; \
+	rm -rf /home/heloufra/data/mysql/* && \
+	rm -rf /home/heloufra/data/wordpress/* \
---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+
+
+
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "create database if not exists ${MYSQL_DATABASE};"
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mysql -h localhost --password=${MYSQL_ROOT_PASSWORD} -e "use mysql;UPDATE user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE user='root';UPDATE user SET plugin = 'mysql_native_password', host = '%' WHERE user = 'root';FLUSH PRIVILEGES;"
+
+
