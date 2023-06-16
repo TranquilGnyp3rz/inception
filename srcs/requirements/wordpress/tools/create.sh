9030e274ee540c5411627e474dff30b6a3de4c8a
heloufra <heloufra@localhost.localdomain>
2023-06-16 18:56:16 -0400

    .

--- srcs/requirements/wordpress/tools/create.sh
+++ srcs/requirements/wordpress/tools/create.sh
@@ -6,9 +6,9 @@ cd /wordpress
 # sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 rm -rf wp-config.php
 wp  config create --allow-root --dbhost="${MYSQL_HOSTNAME}" --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}"
-chmod +x wp-config.php
-wp core install --allow-root --url="${URL_DNS}" --title="${WP_TITLE}" --admin_name="${WP_TITLE}" --admin_password="${WP_ADMIN_PSW}" --admin_email="${WP_ADMIN_EMAIL}"
-wp user create --allow-root ${WP_USR} ${WP_EMAIL} --user_pass=${WP_PWD} --role=${WP_USER_ROLE}
 
+chmod +x wp-config.php
+wp core install --allow-root --url="${URL_DNS}" --title="${WP_TITLE}" --admin_name="${WP_ADMIN}" --admin_password="${WP_ADMIN_PSW}" --admin_email="${WP_ADMIN_EMAIL}"
+wp user create --allow-root ${WP_USR} ${WP_EMAIL} --user_pass=${WP_PWD} --role=${WP_USER_ROLE}
 
 exec php-fpm7.3 -F -R
